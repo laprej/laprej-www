9700e7c2f2133d01f9a6b915c34804d4380fdf3e
Brandon Holt <bholt@cs.washington.edu>
2015-05-01 08:59:05 -0700

    start converting to Generator

--- _plugins/madoko.rb
+++ _plugins/madoko.rb
@@ -2,47 +2,45 @@ require 'fileutils'
 require 'pry'
 
 module Jekyll
-  
-  module Converters
+
+  class MadokoPage < Page
+    def initialize(site, src)
+      @site = site
+      @base = site.source
+      @dir = File.dirname(src)
+      @name = File.basename(src, '.mdk')
+      
+      self.process(@name)
+      self.read_yaml()
+    end
+  end
+
+
+  class MadokoGenerator < Generator
     
-    class Madoko < Converter
+    def generate(site)
       
-      safe true
-      priority :high
       
-      def matches(ext)
-        ext =~ /^\.mdk$/
+      out = ""
+      Tempfile.create(['jekyll','.mdk']) do |t|
+        # convert frontmatter to Madoko...
+        mdk_content = content.gsub(/\A---(.*?)^---$/m,'\\1')
+        puts mdk_content.gsub(/^/,'| ')
+        t.write(mdk_content)
+        t.close
+        cmd = "madoko --fragment --odir=#{File.dirname(t.path)} --verbose #{t.path}"
+        puts "@> #{cmd}"
+        system(cmd)
+        html = t.path.gsub(/\.mdk$/,'.html')
+        puts "@> #{html}"
+        # binding.pry
+        out = File.open(html, 'r').read
+        FileUtils.rm(Dir.glob(t.path.gsub(/.mdk$/,'*')))
       end
       
-      def output_ext(ext)
-        ".html"
-      end
-      
-      def convert(content)
-        # setup
-        
-        out = ""
-        Tempfile.create(['jekyll','.mdk']) do |t|
-          # convert frontmatter to Madoko...
-          mdk_content = content.gsub(/\A---(.*?)^---$/m,'\\1')
-          puts mdk_content.gsub(/^/,'| ')
-          t.write(mdk_content)
-          t.close
-          cmd = "madoko --fragment --odir=#{File.dirname(t.path)} --verbose #{t.path}"
-          puts "@> #{cmd}"
-          system(cmd)
-          html = t.path.gsub(/\.mdk$/,'.html')
-          puts "@> #{html}"
-          # binding.pry
-          out = File.open(html, 'r').read
-          FileUtils.rm(Dir.glob(t.path.gsub(/.mdk$/,'*')))
-        end
-        
-        out
-      end
-      
+      out
     end
     
   end
-  
+
 end
